1e757b934a7f612620ec2e118201259f91f18669
Wbbbb <bird23074035@gmail.com>
2019-07-17 11:46:41 +0800

    ts 練習

---
+++ greeter.ts
@@ -0,0 +1,18 @@
+class Student {
+  fullName: string;
+  constructor (public firstName: string, public middleInitial: string, public lastName: string) {
+    this.fullName = firstName + " " + middleInitial + " " +lastName;
+  }
+}
+
+interface Person {
+  firstName: String;
+  lastName: String;
+}
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+
+document.body.textContent = greeter(user);
