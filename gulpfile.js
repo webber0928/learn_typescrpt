554e1946e373e4aaa22a560524870e4ad0423809
Wbbbb <bird23074035@gmail.com>
2019-07-17 14:58:34 +0800

    gulp 教學完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,35 @@
 var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var tsProject = ts.createProject('tsconfig.json');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var tsify = require('tsify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
+var paths = {
+    pages: ['src/*.html']
+};
 
-gulp.task('default', function (){
-  return tsProject.src()
-    .pipe(tsProject())
-    .js.pipe(gulp.dest('dist'));
+gulp.task('copy-html', function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest('dist'));
 });
+
+gulp.task('default', gulp.series(gulp.parallel('copy-html'), function () {
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/main.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .transform('babelify', {
+        presets: ['es2015'],
+        extensions: ['.ts']
+    })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'));
+}));
